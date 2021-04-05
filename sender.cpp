74f30b9398066126deea62243ab8eb77b56a9a83
mnlsmmdv13 <mnlsmmdv13@protonmail.com>
2021-04-05 09:09:13 +0500

    Code header remake

--- sender.cpp
+++ sender.cpp
@@ -1,13 +1,16 @@
-/*
-Authors: Ahmed Affaan, Ahmed Ahsan Azim
-Country: Maldives
-Language: C++
-Email: mnlsmmdv13@protonmail.com , ahsanazim34@gmail.com
-GitHub/GitLab: https://github.com/mnlsmmdv13 , https://gitlab.uwe.ac.uk/aa2-azim
-Latest Modified: 03/04/2021 16:45PM
-Advice: None
-//*/
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//File Name: sender.cpp                                                                        //
+//File Type: .cpp                                                                              //
+//Language: C++                                                                                //
+//Size: -                                                                                      //
+//Authors: Ahmed Affaan, Ahmed Ahsan Azim                                                      //
+//Country: Republic of Maldives                                                                //
+//Email: mnlsmmdv13@protonmail.com , ahsanazim34@gmail.com                                     //
+//GitHub/GitLab: https://github.com/mnlsmmdv13 , https://gitlab.uwe.ac.uk/aa2-azim             //
+//Last Modified: 03/04/2021 16:45PM                                                            //
+//Description: BBC Micro:bit Challenge 02 Morse Code Sender                                    //
+//Advice: -                                                                                    //
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "MicroBit.h"
 
@@ -16,7 +19,7 @@ MicroBit uBit;
 MicroBitButton buttonB(MICROBIT_PIN_BUTTON_B, MICROBIT_ID_BUTTON_B);
 
 void EchoTime();
-void CLockCalc ();
+void CLockCalc();
 void Terminal();
 bool pressed = false;
 uint64_t StartPoint;
@@ -31,88 +34,81 @@ void EchoTime()
 {
 
     StartPoint = system_timer_current_time();
-
 }
-
 
 /*
  * Function Name: ClockCalc
  * Parameters: N/A
  * Desc: Calculation of time difference
  */
-void ClockCalc() 
+void ClockCalc()
 {
 
-    clockDifference = system_timer_current_time()-StartPoint;
-
+    clockDifference = system_timer_current_time() - StartPoint;
 }
-   
+
 /*
  * Function name: Terminal
  * Parameters: N/A
  * Desc: Displays "-" if button is held between 250-500 seconds.
  *       Displays a "." if button is held for < 250 seconds.
  */
-void Terminal() 
+void Terminal()
 {
 
-          //If-Else statement issued for "." and "-" probabilities
-          if (clockDifference < 250) {
+    //If-Else statement issued for "." and "-" probabilities
+    if (clockDifference < 250)
+    {
 
-            uBit.display.print(".");
-            uBit.sleep(500);
+        uBit.display.print(".");
+        uBit.sleep(500);
+    }
 
-          }
+    else if (clockDifference > 250)
+    {
 
-          else if (clockDifference > 250) {
+        uBit.display.print("-");
+        uBit.sleep(500);
+    }
 
-            uBit.display.print("-");
-            uBit.sleep(500);
-
-          }
-
-          //Clears the screen
-          uBit.display.clear();
-
+    //Clears the screen
+    uBit.display.clear();
 }
 
-//Main function
+//BBC Micro:bit Main function
 int main()
 {
 
     //BBC Micro:bit runtime initialised
     uBit.init();
 
-    while(1) 
+    while (1)
     {
 
-        //Function initialised to recvieve current time
+        //Function initialised to recieve current time
         EchoTime();
-        
+
         //Signal sent to Micro:bit #2 when button B is pressed
-        while (buttonB.isPressed()){
-        pressed = true;
-        uBit.io.P0.setDigitalValue(1);
+        while (buttonB.isPressed())
+        {
+            pressed = true;
+            uBit.io.P0.setDigitalValue(1);
+        }
 
-    }
-    
-    //Requests ClockCalc when transmission ends  
-    uBit.io.P0.setDigitalValue(0);
-    ClockCalc();
+        //Requests ClockCalc when transmission ends
+        uBit.io.P0.setDigitalValue(0);
+        ClockCalc();
 
-    //Display manager requested to show results
-    if(pressed)
-    {
-        Terminal();
+        //Display manager requested to show results
+        if (pressed)
+        {
+            Terminal();
 
-        pressed = false;
-
-    }
+            pressed = false;
+        }
 
         uBit.io.P0.setDigitalValue(0);
-
     }
 
     release_fiber();
-
 }
